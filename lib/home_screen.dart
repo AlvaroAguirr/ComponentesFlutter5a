23b69485801d3a03aca07e696c0d99087f584f33
alvaro aguirre <alguirre2@gmail.com>
2024-02-16 12:54:42 -0600

    05 inserting ListTile in the ListView

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,34 +13,27 @@ class HomeScreen extends StatelessWidget {
       title: const Text("App de componenetes de flutter"),
       ),
         body: ListView(
-          children: const [
-            Text("holas que haces ",style: TextStyle(fontSize: 52),),
-            Text("holas que haces ",style: TextStyle(fontSize: 52),),
-            Text("holas que haces ",style: TextStyle(fontSize: 52),),
-            Text("holas que haces ",style: TextStyle(fontSize: 52),),
-            Text("holas que haces ",style: TextStyle(fontSize: 52),),
-            Text("holas que haces "),
-            Text("holas que haces ",style: TextStyle(fontSize: 52),),
-            Text("holas que haces "),
-            Text("holas que haces ",style: TextStyle(fontSize: 52),),
-            Text("holas que haces "),
-            Text("holas que haces ",style: TextStyle(fontSize: 52),),
-            Text("holas que haces ",style: TextStyle(fontSize: 52),),
-            Text("holas que haces ",style: TextStyle(fontSize: 52),),
-            Text("holas que haces ",style: TextStyle(fontSize: 52),),
-            Text("holas que haces ",style: TextStyle(fontSize: 52),),
-            Text("holas que haces "),
-            Text("holas que haces ",style: TextStyle(fontSize: 52),),
-            Text("holas que haces ",style: TextStyle(fontSize: 52),),
-            Text("holas que haces ",style: TextStyle(fontSize: 52),),
-            Text("holas que haces "),
-            Text("holas que haces "),
-            Text("holas que haces "),
-            Text("holas que haces "),
-            Text("holas que haces "),
-            Text("holas que haces "),
-            Text("holas que haces "),
-           
+          children: const<Widget> [
+            ListTile(
+              title: Text("Entradas"),
+              subtitle: Text("recurperar informacion del teextField"),
+              leading: Icon(Icons.inbox_rounded),
+              trailing: Icon(Icons.arrow_right_outlined),
+            ),
+            Divider(height: 50,),
+            ListTile( 
+              title: Text("Lista infinita"),
+              subtitle: Text("Recuperar muchos elementos"),
+              leading: Icon(Icons.leak_remove_outlined),
+              trailing: Icon(Icons.arrow_right_outlined),
+            ),
+            Divider(),
+            ListTile(
+              title: Text("notificaciones"),
+              subtitle: Text("manejo de notificaciones"),
+              trailing: Icon(Icons.arrow_right_outlined),
+              leading: Icon(Icons.notification_important),
+            )
           ],
         )
  );
